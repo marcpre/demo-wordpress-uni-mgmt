cd3a5dff7a9c588092934e622aed71c8c49a64f8
marcpre <marcus.presich@gmail.com>
2018-03-20 05:59:21 +0000

    adding disable caching trick to functions.php

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,10 +1,13 @@
 <?php
 
 function university_files() {
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    
+    //using microtime to disable caching -> REMOVE when deploying TODO
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    //using microtime to disable caching -> REMOVE when deploying TODO
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
 }
 
 add_action('wp_enqueue_scripts','university_files');
