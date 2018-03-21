9576d41e5b0adf5a16c0f067ded0da6b00b2d8b2
marcpre <marcus.presich@gmail.com>
2018-03-21 14:48:47 +0000

    create dynamic menu items

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -15,6 +15,10 @@ add_action('wp_enqueue_scripts','university_files');
 
 
 function university_features() {
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerMenuOne', 'Footer Location One');
+    register_nav_menu('footerMenuTwo', 'Footer Location Two');
+
     add_theme_support('title-tag');
 }
 
