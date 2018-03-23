e5f3e301ca03389a3ccb087024381139f70e5d4f
marcpre <marcus.presich@gmail.com>
2018-03-23 05:26:57 +0000

    quick fixes

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -25,7 +25,7 @@
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
+            <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php site_url('/blog') ?>" >Blog</a></li>
           </ul>
           
         </nav>
