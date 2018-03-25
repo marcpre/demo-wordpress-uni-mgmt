d21246bd30bee47bbf3cfb82a6ae0c5b1990a2eb
marcpre <marcus.presich@gmail.com>
2018-03-25 09:41:41 +0200

    adding program type

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -24,8 +24,15 @@ function university_features() {
 
 add_action('after_setup_theme','university_features');
 
-// filter posts on archive pages
+// filter posts on all archive pages
 function university_adjust_queries($query)  {
+    if(!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()){
+        $query->set('orderby','title');
+        $query->set('order','ASC');
+        $query->set('posts_per_page',-1);
+       
+    }
+    
     if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
         $today = date('Ymd');
         
