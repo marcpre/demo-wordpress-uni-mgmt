a2a299e44ab4e6aca08ae2fd7041ed09a66b3cfe
marcpre <marcus.presich@gmail.com>
2018-03-27 04:57:22 +0000

    refactor code

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -3,18 +3,15 @@
   get_header();
 
   while(have_posts()) {
-    the_post(); ?>
+    the_post(); 
+    pageBanner(array(
+      'title' => 'Hello, there this is the title.',
+      'subtitle' => 'This is the subtitle', 
+      'photo' => 'http://en.wikipedia.org/wiki/Hill#/media/File:Rolling_Hills_Paranal.jpg'
+      ));
     
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>DONT FORGET TO REPLACE ME LATER</p>
-        </div>
-      </div>  
-    </div>
-
+    ?>
+  
     <div class="container container--narrow page-section">
     
     <?php
