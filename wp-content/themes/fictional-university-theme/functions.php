1cc782b3e8e24d7f9a9e431f06c7fbed3256742f
marcpre <marcus.presich@gmail.com>
2018-03-28 06:02:45 +0000

    create frontend google map

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -30,7 +30,9 @@ function pageBanner($args = NULL) {
   </div>
 <?php }
 
+//load files
 function university_files() {
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDVHkXKIqXgNFqVvkBoB2TmECOoZM8pb8U', NULL, '1.0', true);
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
