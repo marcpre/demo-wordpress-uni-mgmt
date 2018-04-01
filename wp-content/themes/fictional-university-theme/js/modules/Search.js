4d582c0bf19892dfd5abcafd6b7cf74ecc66b379
marcpre <marcus.presich@gmail.com>
2018-04-01 09:28:53 +0200

    fix relative url

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -46,7 +46,7 @@ class Search {
   }
 
   getResults() {
-    $.getJSON("http://localhost/demo-wordpress-uni-mgmt/wp-json/wp/v2/posts/?search=" + this.searchField.val(), posts => {
+    $.getJSON(universityData.root_url + "/wp-json/wp/v2/posts/?search=" + this.searchField.val(), posts => {
       this.resultsDiv.html(`
       <h2 class="search-overlay__section-title" >General Information</h2>
       ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>'}
