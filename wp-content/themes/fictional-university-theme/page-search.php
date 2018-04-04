d35292be6784361e2962e2e585684b4bbb472155
marcpre <marcus.presich@gmail.com>
2018-04-04 04:44:12 +0000

    finalize search

--- wp-content/themes/fictional-university-theme/page-search.php
+++ wp-content/themes/fictional-university-theme/page-search.php
@@ -49,20 +49,13 @@
     <?php } ?>
     
 
-    <div class="generic-content">
-      <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-          <label class="headline headline--medium" for="s">Perform a New Search:</label>
-            <div class="search-form-row">  
-              <input class="s" id="s" placeholder="What are you looking for?" type="search" name="s"/>
-              <input class="search-submit"  type="submit" value="Search"/>
-            </div>
-      </form>
-    </div>
+  get_search_form();
 
   </div>
+  <div class="generic-content">
     
   <?php }
-
   get_footer();
-
 ?>
+
+  </div>
