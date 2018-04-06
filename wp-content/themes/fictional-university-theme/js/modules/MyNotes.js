f04fdd3a6caee74c355b5b86907458627fd6e9c7
marcpre <marcus.presich@gmail.com>
2018-04-06 22:13:01 +0200

    create my notes frontend

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -6,17 +6,41 @@ class MyNotes {
     }
 
     events() {
-        $(".delete.note").on("click", this.deleteNote)
+        $(".delete-note").on("click", this.deleteNote)
+        $(".edit-note").on("click", this.editNote)
     }
 
-    deleteNote() {
+    deleteNote(e) {
+        var thisNote = $(e.target).parents("li")
+        
         $.ajax({
             beforeSend: () => {
               xhr.setRequestHeader('X-WP-Nonce', universityData.nonce)  
             },
-            url: universityData.root_url + '/wp-json/wp/v2/note/' + id, 
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('data-id'), 
             type: 'DELETE', 
             success: (response) => {
+                thisNote.slideUp()
+                console.log(repsonse)
+            }, 
+            error: (response) => {
+                console.log(repsonse)
+            }
+        })
+    }
+    
+    editNote(e) {
+        var thisNote = $(e.target).parents("li")
+        thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field")
+        thisNote.find(".update-note").addClas("update-note--visible")        
+        $.ajax({
+            beforeSend: () => {
+              xhr.setRequestHeader('X-WP-Nonce', universityData.nonce)  
+            },
+            url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('data-id'), 
+            type: 'DELETE', 
+            success: (response) => {
+                thisNote.slideUp()
                 console.log(repsonse)
             }, 
             error: (response) => {
