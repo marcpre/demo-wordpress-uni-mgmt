a4f6ed1eabdf2b98837d3dfbbfdf1f43d23ea4cc
marcpre <marcus.presich@gmail.com>
2018-04-07 10:44:08 +0200

    improve security of frontend

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -143,10 +143,15 @@ function ourLoginTitle() {
   return get_bloginfo('name');
 }
 
-// Force note posts to be private
+// Force note posts to be private and remove html for  subscriber role
 add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate($data) {
+  if($data['post_type'] == "note") {
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_textarea_field($data['post_title']);
+  }
+  
   if($data['post_type'] == "note" AND $data['post_status'] != "trash") {
     $data['post_status'] = "private";
   } 
